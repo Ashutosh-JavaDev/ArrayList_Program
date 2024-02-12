576ec5f0366b1c7c7d73e1d929ac3c2cc1f0dbc1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-12 15:52:26 +0530

    ArrayList Add Color

--- AddColor.java
+++ AddColor.java
@@ -1,11 +1,23 @@
 // Write a Java program to create an array list, add some colors (strings) and print out the collection.
+package ArrayList_Program;
+
 import java.util.Scanner;
 import java.util.ArrayList;
-class ColorAdd{
-    void takingInput(int size){
-        Scanner sc=new Scanner(System.in);
-        ArrayList<String>colour=new ArrayList<>();
+
+class ColorAdd {
+    void takingInput(int size) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> colour = new ArrayList<>();
         System.out.println("How many Color wants to add in the File");
-        size=sc.nextInt();
+        size = sc.nextInt();
+        System.out.println("Please Enter the Name of the color");
+        for (int i = 0; i < size; i++) {
+            colour.add(sc.nextLine());
+        }
+        System.out.println("Given colour Are:   ");
+        for (int i = 0; i < size; i++) {
+            System.out.println(colour.get(i));
+        }
     }
+
 }
