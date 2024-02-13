37d2bad4a1cfc7083c55a57c4d3cbc717cdf3654
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-13 16:01:47 +0530

    Project

--- Copying.java
+++ Copying.java
@@ -6,9 +6,14 @@ class cOpying{
     void cOpY(){
         Scanner sc=new Scanner(System.in);
         ArrayList list=new ArrayList();
-        System.out.println("Enter the value in the List");
+        ArrayList list1=new ArrayList();
+        System.out.println("Enter the value in the List 1");
         for(int i=0;i<5;i++){
             list.add(i);
+        }
+        System.out.println("Enter the value in the List 2");
+        for(int i=0;i<5;i++){
+            list1.add(i);
         }
         
 
