9fb8f08f176402138cba1a827248d992dcdcf8d5
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-13 16:43:42 +0530

    Project

---
+++ rEverse.java
@@ -0,0 +1,19 @@
+package ArrayList_Program;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+class Reverse{
+    void reVerse(){
+        ArrayList<Integer> list=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Value in the List");
+        for(int i=0;i<5;i++){
+            list.add(sc.nextInt());
+        }
+        
+
+    }
+}
+public class rEverse {
+    
+}
