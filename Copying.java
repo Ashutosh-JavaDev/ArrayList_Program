315ac8825332cf5f2b0d7cceaf6feb380b62385b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-13 15:59:49 +0530

    Project

---
+++ Copying.java
@@ -0,0 +1,19 @@
+package ArrayList_Program;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+class cOpying{
+    void cOpY(){
+        Scanner sc=new Scanner(System.in);
+        ArrayList list=new ArrayList();
+        System.out.println("Enter the value in the List");
+        for(int i=0;i<5;i++){
+            list.add(i);
+        }
+        
+
+    }
+}
+public class Copying {
+    
+}
