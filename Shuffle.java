9af655ba320ed58596149acce90a20143e601397
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-13 16:30:05 +0530

    Project

--- Shuffle.java
+++ Shuffle.java
@@ -1,7 +1,23 @@
 package ArrayList_Program;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Collection;
+import java.util.Collections;
+class sHuffle{
+    void shUffle(){
+        Scanner sc=new Scanner(System.in);
+        ArrayList<String>list=new ArrayList<>();
+        System.out.println("Enter the Value in the List");
+        for(int i=0;i<5;i++){
+            list.add(sc.nextLine());
+        }
+        Collections.shuffle(list);
+
+    }
+}
 public class Shuffle {
-    
+    public static void main(String[] args) {
+        sHuffle ob=new sHuffle();
+        ob.shUffle();
+
+    }
 }
