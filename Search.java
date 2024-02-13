268d317dd8245a9e14245ca3aae59aeba20a0f7d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-13 15:44:54 +0530

    Project

--- Search.java
+++ Search.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 import java.util.Collection;
 class srECH{
     void valueContain(){
+        ArrayList list=new ArrayList();
+        list.add("Red");
+        list.add("Green");
+        list.add(23);
+        list.add(2.4f);
+        list.add("Blue");
         
     }
 }
