e9991d9c0445bc20047ed28e96e2d3d0bbe1b644
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-13 15:30:47 +0530

    Project

---
+++ removeElement.java
@@ -0,0 +1,14 @@
+package ArrayList_Program;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+class ElementRemove{
+    void function(){
+        Scanner sc=new Scanner(System.in);
+        ArrayList list=new ArrayList();
+        
+    }
+}
+public class removeElement {
+    
+}
