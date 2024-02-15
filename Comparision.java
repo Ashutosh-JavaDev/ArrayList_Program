cc83d7c4ffd07ac19c1dcb31104a392a097ab5d4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-15 09:32:35 +0530

    Arraylist Project

--- Comparision.java
+++ Comparision.java
@@ -20,7 +20,9 @@ class compared {
         }
         ArrayList<String>c3=new ArrayList<>(); 
         for (String value : lists) {
-            
+            c3.add(lists2.contains(value)?"Yes":"No");
+
+            System.out.println(c3);
         }
     }
 }
