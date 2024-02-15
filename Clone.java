e5914c14d1db17dbc137b18d1cdad27d1be14cfe
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-15 10:04:29 +0530

    Project

--- Clone.java
+++ Clone.java
@@ -1,10 +1,23 @@
 package ArrayList_Program;
+
 import java.util.*;
-class clone{
-    void Clonning(){
-        
+
+class clone {
+    void Clonning() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Value in the List");
+        for (int i = 0; i < 5; i++) {
+            list.add(sc.nextInt());
+        }
+        ArrayList<Integer> lists2 = new ArrayList<Integer>();
+        System.out.println("Enter the Value in the Second ArrayList");
+        for (int i = 0; i < 5; i++) {
+            list.add(sc.nextInt());
+        }
     }
 }
+
 public class Clone {
-    
+
 }
