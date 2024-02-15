c852d00c9466aaab37abaaa15d8a1cf5fb22ccaf
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-15 09:49:46 +0530

    Project

---
+++ Swapping.java
@@ -0,0 +1,17 @@
+package ArrayList_Program;
+import java.util.*;
+class Swap{
+    void  swapping(){
+        ArrayList<Integer>list=new ArrayList<>();
+        Scanner sc=new Scanner((System.in));
+        System.out.println("Enter tthe Value in the List");
+        for(int i=0;i<5;i++){
+            list.add(sc.nextInt());
+        }
+        Collections.swap(list, 0, 3);
+        System.out.println(list);
+    }
+}
+public class Swapping {
+    
+}
