3ec0b6202a85f3ddcee6fabd99c042a537547614
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-15 09:21:49 +0530

    Arraylist Project

--- Comparision.java
+++ Comparision.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 class compared{
-    
+    void comparision(){
+        ArrayList<String>lists=new ArrayList<>();
+        
+    }
 }
 public class Comparision {
     
