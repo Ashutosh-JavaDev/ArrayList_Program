147f952840cd8172cc65c87b13bf004a4ebd8e01
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-15 09:53:44 +0530

    Project

---
+++ Joining.java
@@ -0,0 +1,20 @@
+package ArrayList_Program;
+
+import java.util.*;
+
+class Join {
+    void joinning() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Enter the Valyr in the  List");
+        for (int i = 0; i < 5; i++) {
+            list.add(sc.nextInt());
+        }
+
+        
+    }
+}
+
+public class Joining {
+
+}
