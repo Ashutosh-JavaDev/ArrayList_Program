874f98ba04df1e7c5747eaf04fcb7e8ba86c88a6
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-15 09:23:51 +0530

    Arraylist Project

--- Comparision.java
+++ Comparision.java
@@ -1,13 +1,20 @@
 package ArrayList_Program;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
-class compared{
-    void comparision(){
-        ArrayList<String>lists=new ArrayList<>();
-        
+
+class compared {
+    void comparision() {
+        ArrayList<String> lists = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Value in First List");
+        for (int i = 0; i < 5; i++) {
+            lists.add(sc.nextLine());
+        }
     }
 }
+
 public class Comparision {
-    
+
 }
