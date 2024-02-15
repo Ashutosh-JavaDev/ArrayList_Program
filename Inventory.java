d8c77bafedabd7839c257e691c202f4ceec1cf4e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-15 23:02:17 +0530

    Project

--- Inventory.java
+++ Inventory.java
@@ -12,7 +12,33 @@ class inventory {
         for (int i = 0; i < 5; i++) {
             list1.add(sc.nextInt());
         }
-
+        System.out.println("Press 1: To Remove Items from the Inventory\nPress 2: To Add New Items in the Inventory\nPress 3: To complete Both Task");
+        int Press=sc.nextInt();
+        switch (Press) {
+            case 1:
+                System.out.println("How Many Data want to remove from the List");
+                int value=sc.nextInt();
+                try{
+                    if(value<5&&value>=0){
+                        for(int i=0;i<5;i++){
+                            System.out.println("Enter the Element wants to remove from the List");
+                            int items=sc.nextInt();
+                            list1.remove(items);
+                        }
+                        System.out.println("Update List: "+list1);
+                    }
+                    else{
+                        throw new ArrayIndexOutOfBoundsException("Invalid Position");
+                    }
+                }
+                catch(ArrayIndexOutOfBoundsException e1){
+                    System.out.println(e1);
+                }
+                break;
+        
+            default:
+                break;
+        }
     }
 }
 
