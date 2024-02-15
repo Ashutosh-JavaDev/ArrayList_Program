11424792c3d1e75a3fef3e395461d8d01217f9a8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-15 22:52:46 +0530

    Project

--- Inventory.java
+++ Inventory.java
@@ -3,7 +3,19 @@ package ArrayList_Program;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
-class inventory{}
+
+class inventory {
+    void invent() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Items in the Inventory");
+        for (int i = 0; i < 5; i++) {
+            list1.add(sc.nextInt());
+        }
+
+    }
+}
+
 public class Inventory {
 
 }
