e286d0bff17525818b1c88b9982184a10ab34e30
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-16 22:33:20 +0530

    Project

--- Student.java
+++ Student.java
@@ -1,27 +1,40 @@
 package ArrayList_Program;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
-class informational_Data{
-    void information(){
-        Scanner sc=new Scanner(System.in);
-        ArrayList<String>Name=new ArrayList<>();
-        ArrayList<Integer>Age=new ArrayList<>();
-        ArrayList<String>Course=new ArrayList<>();
+
+class informational_Data {
+    void information() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> Name = new ArrayList<>();
+        ArrayList<Integer> Age = new ArrayList<>();
+        ArrayList<String> Course = new ArrayList<>();
         System.out.println("How Many Student's Data Want to Insert in the Records ?");
-        int Records=sc.nextInt();
+        int Records = sc.nextInt();
         sc.nextLine();
         System.out.println("Enter the Data Properly:");
-        for(int i=0;i<Records;i++){
-            System.out.println("Name of "+(i+1)+" - Student:");
+        for (int i = 0; i < Records; i++) {
+            System.out.println("Name of " + (i + 1) + " - Student:");
             Name.add(sc.nextLine());
-            System.out.println("Age of "+(i+1)+" - Student:");
+            System.out.println("Age of " + (i + 1) + " - Student:");
             Age.add(sc.nextInt());
-            System.out.println("Course of "+(i+1)+" - Student:");
+            System.out.println("Course of " + (i + 1) + " - Student:");
             Course.add(sc.nextLine());
         }
+        for (int i = 0; i < Records; i++) {
+            System.out.print("Name:   " + Name.get(i));
+            System.out.print("Age:   " + Age.get(i));
+            System.out.print("Course:   " + Course.get(i));
+
+        }
+
     }
 }
+
 public class Student {
-    
+    public static void main(String[] args) {
+        informational_Data ob=new informational_Data();
+        ob.information();
+    }
 }
