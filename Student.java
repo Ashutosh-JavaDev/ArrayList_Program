9b5ab4629e0eb8f7ae4cb302fa447a63e2ab81f4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-16 22:24:20 +0530

    Project

--- Student.java
+++ Student.java
@@ -10,6 +10,10 @@ class informational_Data{
         ArrayList<Integer>Classes=new ArrayList<>();
         System.out.println("How Many Student's Data Want to Insert in the Records ?");
         int Records=sc.nextInt();
+        System.out.println("Enter the Data Properly:");
+        for(int i=0;i<Records;i++){
+            
+        }
     }
 }
 public class Student {
