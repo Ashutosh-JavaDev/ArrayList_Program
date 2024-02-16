343897411a90cdf261529cadd28153f535fc0284
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-16 22:29:35 +0530

    Project

--- Student.java
+++ Student.java
@@ -6,13 +6,19 @@ class informational_Data{
     void information(){
         Scanner sc=new Scanner(System.in);
         ArrayList<String>Name=new ArrayList<>();
-        ArrayList<String>Age=new ArrayList<>();
-        ArrayList<Integer>Classes=new ArrayList<>();
+        ArrayList<Integer>Age=new ArrayList<>();
+        ArrayList<String>Course=new ArrayList<>();
         System.out.println("How Many Student's Data Want to Insert in the Records ?");
         int Records=sc.nextInt();
+        sc.nextLine();
         System.out.println("Enter the Data Properly:");
         for(int i=0;i<Records;i++){
-            
+            System.out.println("Name of "+(i+1)+" - Student:");
+            Name.add(sc.nextLine());
+            System.out.println("Age of "+(i+1)+" - Student:");
+            Age.add(sc.nextInt());
+            System.out.println("Course of "+(i+1)+" - Student:");
+            Course.add(sc.nextLine());
         }
     }
 }
