70f1e14d586dd34548ce9059bb21e7d5da207c8e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-16 22:18:20 +0530

    Project

---
+++ Student.java
@@ -0,0 +1,12 @@
+package ArrayList_Program;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+class informational_Data{
+    void information(){
+        
+    }
+}
+public class Student {
+    
+}
