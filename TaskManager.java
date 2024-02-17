2f1f9c47fe201914dbf4f2710d102082c22d664e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-17 12:44:25 +0530

    Project

--- TaskManager.java
+++ TaskManager.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 class addTask{
-    
+    void taskManager(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("How many Task You Want add in you list for a Today");
+        int task=sc.nextInt();
+        ArrayList<String>AddingTask=new ArrayList<>();
+        System.out.println("Add Task in Your List:\nSyntax:\n1.Your Task\n2.Your Task\n3.Your Task\netc... ");
+        for(int i=0;i<task;i++){
+            AddingTask.add(sc.nextLine());
+        }
+        System.out.println("Task Added SuccesFully");
+        System.out.println("How Many Task Did You Completed Yet ?");
+    }
 }
 public class TaskManager {
     
