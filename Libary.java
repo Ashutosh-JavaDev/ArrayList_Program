d13235bed151b3b7502514a95d5e6b2caedde556
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-18 16:15:58 +0530

    Project

---
+++ Libary.java
@@ -0,0 +1,20 @@
+package ArrayList_Program;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+class BookInformation{
+    void information(){
+        Scanner sc=new Scanner(System.in);
+        ArrayList<String>bookTitle=new ArrayList<>();
+        ArrayList<String>Author=new ArrayList<>();
+        System.out.println("How Many new Books Want to Add in the Libary ? ");
+        int bookCount=sc.nextInt();
+        System.out.println("Enter the Title and Author Name of the Book.");
+        for(int i=0;i<bookCount;i++){
+            
+        }
+    }
+}
+public class Libary {
+    
+}
