613054a572333629002a7d366e026472c1f72ed9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-19 21:15:01 +0530

    Project

---
+++ Contact.java
@@ -0,0 +1,24 @@
+package ArrayList_Program;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class information{
+    void addInfo(){
+        Scanner sc=new Scanner(System.in);
+        ArrayList<String>Name=new ArrayList<>();
+        ArrayList<Integer>Number=new ArrayList<>();
+        System.out.println("How many Data Want to insert in the Records");
+        int Data=sc.nextInt();
+        System.out.println("Add Contacts Details");
+        for(int i=0;i<Data;i++){
+            
+        }
+    }
+}
+class search extends information{
+
+}
+public class Contact {
+    
+}
