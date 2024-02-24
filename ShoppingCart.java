eb6d7e47d78d769ffe6296c41a9df0cd98292f2f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-24 08:21:25 +0530

    Project

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,10 +1,17 @@
 package ArrayList_Program;
+
 import java.util.Scanner;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-class Shoping{
-    ArrayList<String>item=new ArrayList<>();
-    ArrayList<Integer>Quantitiy=new ArrayList<>();
-}public class ShoppingCart {
-    
+
+class Shoping {
+    ArrayList<String> item = new ArrayList<>();
+    ArrayList<Integer> Quantitiy = new ArrayList<>();
+    void addItems(){
+        
+    }
+}
+
+public class ShoppingCart {
+
 }
