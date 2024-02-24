ac8c98b74743fa8416e2611e713ed1e60ed12676
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-24 09:21:42 +0530

    Project

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 class Shoping {
     ArrayList<String> item = new ArrayList<>();
     ArrayList<Integer> Quantity = new ArrayList<>();
+    ArrayList<Integer>cost=new ArrayList<>();
     Scanner sc = new Scanner(System.in);
     void addItems() {
         System.out.println("Press Stop for stop Adding item in the Cart");
@@ -18,16 +19,26 @@ class Shoping {
         if(itemsName.equalsIgnoreCase("Stop")){
             break;
         }
-        System.out.println("Items Quantity: ");
+        System.out.println(itemsName+" Quantity: ");
         int Quant=sc.nextInt();
         sc.nextLine();
+        int costing;
+        System.out.println("Cost of 1 Quantity: ");
+        costing=sc.nextInt();
         item.add(itemsName);
         Quantity.add(Quant);
+        cost.add(costing*Quant);
       }
         System.out.println("Items Add in the Shop:  ");
         for (int i = 0; i < Quantity.size(); i++) {
-            System.out.println("Item Name:  "+item.get(i)+" Qunatity:   "+Quantity.get(i));
+            System.out.println("Item Name: "+item.get(i)+" : Qunatity: "+Quantity.get(i)+" Total Cost: "+cost.get(i));
         }
+    }
+    void removes(){
+        
+    }
+    void switchCondition(){
+        System.out.println("Press 1: To Remove Items From the List\nPress 2: To Update Quantities Press 3:To Display Records")
     }
 }
 
