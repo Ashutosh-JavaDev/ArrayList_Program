f05b9190cb166a03fd3602df9642f909d2d0ad93
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-24 08:48:32 +0530

    Project

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -11,18 +11,19 @@ class Shoping {
     void addItems() {
         System.out.println("Press Stop for stop Adding item in the Cart");
         String str="";
-        boolean state=true;
         System.out.println("Add Items in the List:  ");
-        while (state) {
-            for (int i = 0; i < Quantity.size(); i++) {
-                System.out.println("Item Name:  " + item.add(sc.nextLine()) + " Quantity:  " + Quantity.add(sc.nextInt()));
-                if(str.equalsIgnoreCase("Stop")){
-                    state=false;
-                    break;
-                }
-               
-            }
+      while(true){
+        System.out.println("Items Name: ");
+        String itemsName=sc.nextLine();
+        if(itemsName.equalsIgnoreCase("Stop")){
+            break;
         }
+        System.out.println("Items Quantity: ");
+        int Quant=sc.nextInt();
+        sc.nextLine();
+        item.add(itemsName);
+        Quantity.add(Quant);
+      }
         System.out.println("Items Add in the Shop:  ");
         for (int i = 0; i < Quantity.size(); i++) {
             System.out.println("Item Name:  "+item.get(i)+" Qunatity:   "+Quantity.get(i));
