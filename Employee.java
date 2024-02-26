1a91761062c0d1f277aaab328ed6c0e7bb7ce140
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-26 11:13:37 +0530

    Project

--- Employee.java
+++ Employee.java
@@ -1,6 +1,7 @@
 package ArrayList_Program;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class employee {
@@ -20,9 +21,14 @@ class employee {
             } else {
                 EmpName.add(empName);
                 System.out.print("EmpID:  ");
+                try{
                 int empId = sc.nextInt();
                 EmpID.add(empId);
-                sc.nextLine();
+                sc.nextLine(); 
+                }
+                catch(InputMismatchException e){
+                    System.out.println(e);
+                }
                 System.out.print("Employee Degination:  ");
                 String empDeg = sc.nextLine();
                 EmpDegination.add(empDeg);
@@ -34,6 +40,23 @@ class employee {
             System.out.println("Employee "+(i+1)+":");
             System.out.print("Name: " + EmpName.get(i) + " \nEmployee ID: " + EmpID.get(i) + "\nEmployee Deg: "
                     + EmpDegination.get(i) + "\n");
+        }
+    }
+
+    void RemoveEmp() {
+        int indextoRemove = -1;
+        System.out.println("Enter the Emoplyee Name, whose Details want to remove ?");
+        String employeeName = sc.nextLine();
+        for(int i=0;i<EmpName.size();i++){
+            if(employeeName.equalsIgnoreCase(EmpName.get(i))){
+                indextoRemove=i;
+                break;
+            }
+        }
+        if(indextoRemove!=-1){
+          EmpName.remove(indextoRemove);
+          EmpID.remove(indextoRemove);
+          EmpDegination.remove(indextoRemove);   
         }
     }
 }
