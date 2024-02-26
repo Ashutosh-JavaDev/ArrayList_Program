1fdda5f59f74cd3989f04b03a321d0b1e98eabaa
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-26 10:36:35 +0530

    Project

--- Employee.java
+++ Employee.java
@@ -7,36 +7,38 @@ class employee {
     ArrayList<String> EmpName = new ArrayList<>();
     ArrayList<Integer> EmpID = new ArrayList<>();
     ArrayList<String> EmpDegination = new ArrayList<>();
-    Scanner sc=new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
+
     void addEmployee() {
         System.out.println("Press Enter or Stop to stop adding Details");
         System.out.println("Add Employee Details:");
-        while(true){
+        while (true) {
             System.out.print("Employee Name: ");
-            String empName=sc.nextLine();
-            if(empName.equalsIgnoreCase("Stop")|| empName.equalsIgnoreCase("")){
+            String empName = sc.nextLine();
+            if (empName.equalsIgnoreCase("Stop") || empName.equalsIgnoreCase("")) {
                 break;
-            }
-            else{
+            } else {
                 EmpName.add(empName);
                 System.out.print("EmpID:  ");
-                int empId=sc.nextInt();
+                int empId = sc.nextInt();
                 EmpID.add(empId);
                 sc.nextLine();
                 System.out.print("Employee Degination:  ");
-                String empDeg=sc.nextLine();
+                String empDeg = sc.nextLine();
                 EmpDegination.add(empDeg);
             }
         }
-      for(int i=0;i<EmpName.size();i++){
-        System.out.print("Name: "+EmpName.get(i)+" \nEmployee ID: "+EmpID.get(i)+"\nEmployee Deg: "+EmpDegination.get(i)+"\n");
-      }
+        System.out.println("Employee Details:");
+        for (int i = 0; i < EmpName.size(); i++) {
+            System.out.print("Name: " + EmpName.get(i) + " \nEmployee ID: " + EmpID.get(i) + "\nEmployee Deg: "
+                    + EmpDegination.get(i) + "\n");
+        }
     }
 }
 
 public class Employee {
     public static void main(String[] args) {
-        employee  ob=new employee();
+        employee ob = new employee();
         ob.addEmployee();
     }
 }
