e05e1d1ed704db077292e7bf2a9f30774b3de603
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-26 09:57:16 +0530

    Project

--- Employee.java
+++ Employee.java
@@ -7,9 +7,15 @@ class employee {
     ArrayList<String> EmpName = new ArrayList<>();
     ArrayList<Integer> EmpID = new ArrayList<>();
     ArrayList<String> EmpDegination = new ArrayList<>();
-
+    Scanner sc=new Scanner(System.in);
     void addEmployee() {
-
+        System.out.println("Press Enter or Stop to stop adding Details");
+        System.out.println("Add Employee Details:");
+        while(true){
+            System.out.print("Employee Name: ");
+            String empName=sc.nextLine();
+            
+        }
     }
 }
 
