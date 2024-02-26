010b3c0d9b93189ccdd6a9b85d440e88a55af1c3
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-26 09:52:08 +0530

    Project

--- Employee.java
+++ Employee.java
@@ -1,9 +1,18 @@
 package ArrayList_Program;
+
 import java.util.ArrayList;
 import java.util.Scanner;
-class employee{
-    
+
+class employee {
+    ArrayList<String> EmpName = new ArrayList<>();
+    ArrayList<Integer> EmpID = new ArrayList<>();
+    ArrayList<String> EmpDegination = new ArrayList<>();
+
+    void addEmployee() {
+
+    }
 }
+
 public class Employee {
-    
+
 }
