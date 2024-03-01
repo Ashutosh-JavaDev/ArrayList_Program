f482d710df10de0d1fe8448e4449575126871429
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-01 15:31:22 +0530

    Project

--- Recipe.java
+++ Recipe.java
@@ -1,12 +1,28 @@
 package ArrayList_Program;
+
 import java.util.Scanner;
 import java.util.ArrayList;
-class recipe{
-    Scanner sc=new Scanner(System.in);
-    void Recipes(){
-        System.out.println("");
+
+class recipe {
+    ArrayList<String> recipesName = new ArrayList<>();
+    ArrayList<Integer> recipePrice = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    void Recipes(ArrayList<String> recipesNAme, ArrayList<Integer> recipePrice) {
+        this.recipePrice = recipePrice;
+        this.recipesName = recipesNAme;
     }
+
+    ArrayList<String> returnName() {
+        return recipesName;
+    }
+
+    ArrayList<Integer> returnPrice() {
+        return recipePrice;
+    }
+
 }
+
 public class Recipe {
-    
+
 }
