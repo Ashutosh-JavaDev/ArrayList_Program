84f7a3d346a7c89f19d941e68db57a67406320a3
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-03 09:50:00 +0530

    Project

--- Ingredient.java
+++ Ingredient.java
@@ -1,21 +1,31 @@
 package ArrayList_Program;
-import java. util.ArrayList;
+
+import java.util.ArrayList;
 import java.util.Scanner;
-class Recipe{
-    ArrayList<String>Iname=new ArrayList<>();
-    ArrayList<Integer>iPrice=new ArrayList<>();
-    Scanner sc=new Scanner(System.in);
-    void addingValue(ArrayList<String>Sname,ArrayList<Integer>Sprice){
-        this.Iname=Sname;
-        this.iPrice=Sprice;
+
+class Recipe {
+    ArrayList<String> Iname = new ArrayList<>();
+    ArrayList<Integer> iPrice = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    void addingValue(ArrayList<String> Sname, ArrayList<Integer> Sprice) {
+        this.Iname = Sname;
+        this.iPrice = Sprice;
         System.out.println("Press Enter to Stop Adding Value in The List");
         System.out.println("Add Ingredient Name");
-        String name=sc.nextLine();
-        if(name.equals("")){
-            
+        System.out.print("Ingredient: ");
+        String name = sc.nextLine();
+        if (name.equals("")) {
+            return;
+        } else {
+            Sname.add(name);
+            System.out.print("Price: ");
+            int price = sc.nextInt();
+            Sprice.add(price);
         }
     }
 }
+
 public class Ingredient {
-    
+
 }
