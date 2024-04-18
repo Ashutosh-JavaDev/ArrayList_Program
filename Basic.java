cc7c42249d1a864c9b5023899e27d2c36e4cfabf
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-18 23:50:20 +0530

    ArrayList

--- Basic.java
+++ Basic.java
@@ -5,10 +5,15 @@ import java.util.ArrayList;
 
 class inserting {
     public void insert() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter number or write 'STOP' to stop adding number");
-
         while (true) {
-
+            String number = sc.nextLine();
+            if (number.equalsIgnoreCase("STOP")) {
+                break;
+            } else {
+                float num = Float.parseFloat(number);
+            }
         }
     }
 }
