0e0169f16274beef8c7641672994df5ec9739086
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-18 23:47:25 +0530

    ArrayList

--- Basic.java
+++ Basic.java
@@ -2,9 +2,14 @@ package ArrayList_Program;
 
 import java.util.Scanner;
 import java.util.ArrayList;
-class inserting{
-    public void insert(){
-        
+
+class inserting {
+    public void insert() {
+        System.out.println("Enter number or write 'STOP' to stop adding number");
+
+        while (true) {
+
+        }
     }
 }
 
