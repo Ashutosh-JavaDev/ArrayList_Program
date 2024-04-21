7f49985c1aafdc9ec947a32f2bf21d8a8b0481c3
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-21 13:46:38 +0530

    ArrayList

--- Basic.java
+++ Basic.java
@@ -4,62 +4,72 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 
-class inserting {
+class Inserting {
     public void insert() {
         Scanner sc = new Scanner(System.in);
-        ArrayList list = new ArrayList<>();
-        System.out.println("Enter number or Press Double time 'ENTER' to stop adding number");
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Enter numbers or press Enter twice to stop adding numbers:");
         while (true) {
-            String number = sc.nextLine();
-            if (number.equals("")) {
+            String input = sc.nextLine();
+            if (input.isEmpty()) {
                 break;
             } else {
                 try {
-                    int num = Integer.parseInt(number);
+                    int num = Integer.parseInt(input);
                     list.add(num);
                 } catch (NumberFormatException e) {
                     System.out.println("Invalid input. Please enter a valid integer.");
                 }
             }
+        }
+        sc.close(); // Close the Scanner to prevent resource leak
 
-        }
-        int sum=0;
-        for(int i=0;i<list.size();i++){
-            sum=sum+list.get(i);
-        }
-        System.out.println(sum);
-        System.out.println("Numbers are:");
+        int sum = 0;
         for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+            sum += list.get(i);
         }
-        System.out.println("Enter the Index want to remove:");
-        int ind = sc.nextInt();
-        list.remove(ind - 1);
-        System.out.println("Existing Numbers are:");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        System.out.println("Sum of numbers: " + sum);
+        
+        System.out.println("Numbers in the list:");
+        for (int num : list) {
+            System.out.println(num);
         }
-        System.out.println("Enter the Number want to find:");
-        int find = sc.nextInt();
-        if (list.contains(find)){
-            System.out.println(true);
+
+        if (!list.isEmpty()) {
+            System.out.println("Enter the index of the number you want to remove:");
+            int ind = sc.nextInt();
+            if (ind >= 1 && ind <= list.size()) { // Check if index is within bounds
+                list.remove(ind - 1);
+                System.out.println("List after removing the element:");
+                for (int num : list) {
+                    System.out.println(num);
+                }
+            } else {
+                System.out.println("Invalid index.");
+            }
+            
+            System.out.println("Enter the number you want to find:");
+            int find = sc.nextInt();
+            System.out.println("List contains " + find + ": " + list.contains(find));
+            
+            System.out.println("Reversed List:");
+            Collections.reverse(list);
+            for (int num : list) {
+                System.out.println(num);
+            }
+            
+            System.out.println("Shuffled List:");
+            Collections.shuffle(list);
+            System.out.println(list);
+        } else {
+            System.out.println("List is empty.");
         }
-        else{
-            System.out.println(false);
-        }
-        System.out.println("Reverse List:");
-        for(int i=list.size()-1;i>=0;i--){
-           System.out.println(list.get(i)); 
-        }
-        Collections.shuffle(list);
-        System.out.println(list);
-    
     }
 }
 
 public class Basic {
     public static void main(String[] args) {
-        inserting ob = new inserting();
+        Inserting ob = new Inserting();
         ob.insert();
     }
 }
