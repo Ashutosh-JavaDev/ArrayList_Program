e56c898529029ced156faa55e098da3b130d6ca0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-16 21:57:40 +0530

    ArrayList Project

--- StudentDatabase.java
+++ StudentDatabase.java
@@ -1,10 +1,12 @@
 package ArrayList_Program;
 import java.util.ArrayList;
+import java.util.Scanner;
 class AddData{
     public void addData(){
         ArrayList<String>Name=new ArrayList<>();
         ArrayList<Integer>Class=new ArrayList<>();
-        ArrayList<Integer>marks=new ArrayList<>();
+        ArrayList<Integer>Marks=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
     }
 }
 public class StudentDatabase {
