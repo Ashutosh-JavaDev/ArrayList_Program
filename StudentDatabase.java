ffe88f91462c35f8bd73873c9fd79d0e82fa83d0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-17 13:00:51 +0530

    ArrayList

--- StudentDatabase.java
+++ StudentDatabase.java
@@ -34,6 +34,7 @@ class AddData {
             System.out.println("Enter Marks of 5 Subjects");
             for (int i = 0; i < 5; i++) {
                 try {
+                    System.out.print("Enter marks for subject " + (i + 1) + ": ");
                     int marks = sc.nextInt();
                     mark.add(marks);
                 } catch (InputMismatchException e) {
@@ -45,22 +46,27 @@ class AddData {
             Marks.add(mark);
             sc.nextLine();
         }
-
+        Display disp = new Display();
+        disp.display();
     }
 }
 
 class Display extends AddData {
-    void display() {
+    public void display() {
         System.out.println(
                 "Name\tAge\tMath\tEng\tIT\tOS\tDS\n--------------------------------------------------------------");
         for (int i = 0; i < Name.size(); i++) {
-            System.out.println("Student" + (i + 1) + ":..................................... ");
+            System.out.println("Student" + (i + 1) + ": ..................................... ");
+            System.out.println("Name: " + Name.get(i));
+            System.out.println("Age: " + Age.get(i));
+            System.out.print("Marks: ");
 
-            System.out.println("Name: " + Name.get(i) + "\nAge: " + Age.get(i) + "\nMarks: "
-                    + Marks.get(i) + "\n----------------------------");
+            // Print marks for 5 subjects
+            for (int mark : Marks.get(i)) {
+                System.out.print(mark + "\t");
+            }
+            System.out.println("\n----------------------------");
         }
-
-        System.out.println();
     }
 }
 
